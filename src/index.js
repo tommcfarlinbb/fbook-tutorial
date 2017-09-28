a7cb8a99eaf91d15bfa795a57253179692226858
Tom McFarlin <tom.mcfarlin@brandingbrand.com>
2017-09-28 13:32:31 -0400

    change value of object

--- src/index.js
+++ src/index.js
@@ -3,61 +3,34 @@ import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
 // add counter
-const addCounter = (list, index) => {
-    return [
-        ...list.slice(0, index),
-        0,
-        ...list.slice(index)
-    ];
-}
-
-// remove counter
-const removeCounter = (list, index) => {
-    return [
-        ...list.slice(0, index),
-        ...list.slice(index + 1)
-    ];
-}
-
-// increment counter
-const incrementCounter = (list, index) => {
-    return [
-        ...list.slice(0, index),
-        list[index] + 1,
-        ...list.slice(index + 1)
-    ];
+const toggleToDoCheckbox = (todo) => {
+    // proposed for ES7
+    // return {
+        //     ...todo,
+        //     checkbox: !todo.checkbox,
+        // }
+        
+    return Object.assign({}, todo, {checkbox: !todo.checkbox});
 }
 
 // tests
-const testAddCounter = () => {
-    const listBefore = [1, 2];
-    const listAfter = [1, 0, 2];
+const testToggleToDoCheckbox = () => {
+    const todoBefore = {
+        title: 'My To Do List',
+        description: 'To Do List Description',
+        checkbox: false,
+    };
+    const todoAfter = {
+        title: 'My To Do List',
+        description: 'To Do List Description',
+        checkbox: true,
+    };
 
-    deepFreeze(listBefore)
-    expect(addCounter(listBefore, 1))
-        .toEqual(listAfter);
+    deepFreeze(todoBefore)
+    expect(toggleToDoCheckbox(todoBefore))
+        .toEqual(todoAfter);
 };
-testAddCounter();
-console.log("add counter test passed")
+testToggleToDoCheckbox();
+console.log("toggle todo checkbox test passed")
 
-const testRemoveCounter = () => {
-    const listBefore = [1, 0, 2];
-    const listAfter = [1, 2];
-
-    deepFreeze(listBefore)
-    expect(removeCounter(listBefore, 1))
-        .toEqual(listAfter);
-};
-testRemoveCounter();
-console.log("remove counter test passed")
-
-const testIncrementCounter = () => {
-    const listBefore = [0, 1, 3];
-    const listAfter = [0, 2, 3];
-
-    deepFreeze(listBefore)
-    expect(incrementCounter(listBefore, 1))
-        .toEqual(listAfter);
-};
-testIncrementCounter();
-console.log("increment counter test passed")
+console.log("tests passed")
